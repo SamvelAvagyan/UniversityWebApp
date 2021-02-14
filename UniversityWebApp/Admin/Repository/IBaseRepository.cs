23239d98228e888f3398387f79e140c715521a3d
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-02-14 05:29:26 -0800

    Add BaseRepository

---
+++ UniversityWebApp/Admin/Repository/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IBaseRepository<T>
+    {
+        Task<IQueryable<T>> GetAllAsync();
+        void AddAsync(T model);
+        void DeleteAsync(int id);
+        void UpdateAsync(T model);
+    }
+}
---
+++ UniversityWebApp/Admin/Repository/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Repository.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T>
+        where T : BaseModel
+    {
+        private readonly UniversityDbContext dbContext;
+
+        public BaseRepository(UniversityDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async void AddAsync(T model)
+        {
+            dbContext.Set<T>().Add(model);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async void DeleteAsync(int id)
+        {
+            dbContext.Set<T>().Find(id).Active = false;
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            return await Task.Run(() =>
+            {
+                return dbContext.Set<T>();
+            });
+        }
+
+        public async void UpdateAsync(T model)
+        {
+            model.ModifiedOn = DateTime.Now;
+            dbContext.Set<T>().Update(model);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
