add1a1d46f6c366636e1f7b019e45b613d6093a8
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-02-20 09:03:55 -0800

    Implement get and set for Mark property of Student

--- UniversityWebApp/Admin/Repository/Models/Student.cs
+++ UniversityWebApp/Admin/Repository/Models/Student.cs
@@ -2,8 +2,19 @@
 {
     public class Student : BaseModel
     {
+        private byte mark;
         public string Name { get; set; }
         public string Surname { get; set; }
-        public byte Mark { get; set; }
+        public byte Mark
+        {
+            get { return mark; }
+            set
+            {
+                if (value > 0 && value < 21)
+                {
+                    mark = value;
+                }
+            }
+        }
     }
 }
