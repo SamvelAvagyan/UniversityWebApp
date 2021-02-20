88a8f5c7c0514f776858be3f64b78e5ff772894d
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-02-20 08:54:32 -0800

    Add TeacherController and Index View

---
+++ UniversityWebApp/Admin/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using System.Threading.Tasks;
+
+namespace Admin.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ITeacherRepository teacherRepository;
+
+        public TeacherController(ITeacherRepository teacherRepository)
+        {
+            this.teacherRepository = teacherRepository;
+        }
+
+        // GET: TeacherController
+        public async Task<ActionResult> Index()
+        {
+            return View(await teacherRepository.GetActivesAsync());
+        }
+
+        // GET: TeacherController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TeacherController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TeacherController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TeacherController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TeacherController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TeacherController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TeacherController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
