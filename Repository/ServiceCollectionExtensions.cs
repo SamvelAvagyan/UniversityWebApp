c8851a83637ae7ae90eba88cb62ba9dea39c6906
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-03-08 10:49:01 -0800

    Delete Repository.DependencyInjection Project And Replace ServiceCollectionExtension to Repository project

---
+++ Repository/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository;
+using Repository.Impl;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<UniversityDbContext>(con => con.UseSqlServer(connectionString));
+            services.AddScoped<DbContext, UniversityDbContext>();
+            return services;
+        }
+
+        public static IServiceCollection AddRepositoryContracts(this IServiceCollection services)
+            => services
+                .AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>))
+                .AddScoped<IStudentRepository, StudentRepository>()
+                .AddScoped<ITeacherRepository, TeacherRepository>()
+                .AddScoped<IUniversityRepository, UniversityRepository>();
+
+        public static IServiceCollection AddRepository(this IServiceCollection services, string connectionString)
+            => services
+                .AddDbContext(connectionString)
+                .AddRepositoryContracts();
+    }
+}
